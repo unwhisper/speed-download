8d20f7de314b5198024a4d3f843692b11f9df3f9
unwhisper <591856607@qq.com>
2021-11-01 12:37:30 +0800

    add

---
+++ README.md
@@ -0,0 +1,49 @@
+# speed-down
+
+nodejs 大文件分片下载工具，使用进程process的方式分片下载，文件合并使用filestream，不受nodejs单进程的内存限制可以下载较大的文件。
+
+## 用法
+
+## 安装
+
+全局安装
+
+```
+npm install speed-down
+```
+如果要全局安装请加上-g参数
+
+全局安装模式下，可以直接当做下载工具使用：
+
+```
+speed-down "http://xxx/big-file.zip" big-file.zip 4
+```
+表示４个分片并发下载
+
+在项目中引用：
+
+```
+const speed-down = require('speed-down');
+
+(async() => {
+        var url = 'http://bla..../file.mp4';
+        var filepath = 'filename.mp4';
+        var con_num = 4;
+
+        let stime = new Date().getTime();
+        console.log('start download, concurrency: ' + con_num);
+
+        var downloader = new speed-down.Downloader(url, filepath, {
+            'concurrency': con_num,
+            'progress_throttle': 4000
+        });
+
+        downloader.onProgress((pct, tinfo, pinfo) => {
+            console.log('progress:',pct);
+        });
+
+        let ret = await downloader.download();
+
+        console.log('download ' + (ret ? 'success' : 'fail') + ', cost: ' + (new Date().getTime() - stime) + 'ms');
+})();
+```
---
+++ sub-download.js
@@ -0,0 +1,67 @@
+const request = require('request');
+const progress = require('request-progress');
+const fs = require('fs');
+
+process.send = process.send || function(msg) {
+    console.log(msg)
+};
+
+const download = (key, url, headers, filepath, range_start, range_end, throttle) => {
+    let d_err;
+    let header = Object.assign({
+        'Range': 'bytes=' + range_start + '-' + range_end
+    }, JSON.parse(headers));
+    let r = request({
+        uri: url, 
+        rejectUnauthorized: false,
+        headers: header
+    });
+    progress(r, {
+            'throttle': throttle
+        })
+        .on('response', response => {
+            if (response.statusCode < 200 || response.statusCode > 210) {
+                r.abort();
+                d_err = 'http status code: ' + response.statusCode;
+                process.send({
+                    'key': key,
+                    'err': d_err
+                });
+            }
+        })
+        .on('progress', function(state) {
+            process.send({
+                'key': key,
+                'progress': state
+            });
+        })
+        .on('error', function(err) {
+            d_err = err;
+            process.send({
+                'key': key,
+                'err': err
+            });
+        })
+        .on('end', function() {
+            process.send({
+                'key': key,
+                'finish': true,
+                'err': d_err
+            });
+        })
+        .pipe(fs.createWriteStream(filepath));
+}
+
+if (require.main !== module) {
+    exports.download = download;
+} else {
+    if (process.argv.length < 9) {
+        console.error('invalidate process arguments');
+    } else {
+        let [_1, _2, key, url, headers, filepath, range_start, range_end, throttle] = process.argv;
+        range_start = parseInt(range_start);
+        range_end = parseInt(range_end);
+        throttle = parseInt(throttle);
+        download(key, url, headers, filepath, range_start, range_end, throttle);
+    }
+}
